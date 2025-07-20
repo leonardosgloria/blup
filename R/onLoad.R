5be147f722041042b73b11ff81b1be639ef46c10
Leonardo Gloria <78810758+leonardosgloria@users.noreply.github.com>
2025-07-20 06:39:21 -0500

    Update onLoad.R

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,4 +1,4 @@
 .onLoad <- function(libname, pkgname) {
-  #download_BLUPF90(dest_folder = paste0(.libPaths()[1],"/blupf90"))
+  download_BLUPF90(dest_folder = paste0(.libPaths()[1],"/blupf90"))
   print("download blupf90 software")
 }
